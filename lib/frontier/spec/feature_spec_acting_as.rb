20c0c42dab2a35c34a0dfcd5e0d98486b2838d12
Benjamin Robinson <benjaminrobinson1313@gmail.com>
2018-01-03 22:23:01 +0800

    Update acting_as feature spec generation

--- lib/frontier/spec/feature_spec_acting_as.rb
+++ lib/frontier/spec/feature_spec_acting_as.rb
@@ -9,11 +9,11 @@ class Frontier::Spec::FeatureSpecActingAs
   # Render common field inputs if object is acting as a product from MsCore.
   def to_s
     raw = <<-STRING
-fill_in('#{model.name.as_singular}[name_en]',        with: #{model.name.as_singular}_attributes[:name])
-fill_in('#{model.name.as_singular}[description_en]', with: #{model.name.as_singular}_attributes[:description])
-fill_in('#{model.name.as_singular}[hopoff_url_en]',  with: #{model.name.as_singular}_attributes[:hopoff_url])
+fill_in('#{model.name.as_singular}[name_en]',        with: #{model.name.as_singular}_attributes[:name_en])
+fill_in('#{model.name.as_singular}[description_en]', with: #{model.name.as_singular}_attributes[:description_en])
+fill_in('#{model.name.as_singular}[hopoff_url_en]',  with: #{model.name.as_singular}_attributes[:hopoff_url_en])
 within('.#{model.name.as_singular}_provider_id') do
-  semantic_select(selector: '##{model.name.as_singular}_provider_id', value: provider.id)
+  semantic_select(selector: '##{model.name.as_singular}_provider_id', value: provider.id.to_s)
 end
 STRING
     raw.rstrip
