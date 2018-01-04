b4eccf1f7558cef5a0d1828d546996ce9b662b1a
Benjamin Robinson <benjaminrobinson1313@gmail.com>
2018-01-04 10:40:08 +0800

    Update landing page routing spec.

--- lib/generators/frontier_landing_controller/templates/landing_page_routing_spec.rb
+++ lib/generators/frontier_landing_controller/templates/landing_page_routing_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe '<%= model.engine_name.camelize %>::PagesController routing' do
   routes { <%= model.engine_name.camelize %>::Engine.routes }
 
-  subject { get :landing }
+  subject { get '/<%= model.engine_name %>' }
   it 'routes pages#landing as expected' do
     expect(subject).to route_to(
       controller: '<%= model.engine_name %>/pages',
